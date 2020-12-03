9eae21aaaf978b241fb888bc75c2c66c66d408a7
Andre McCurdy <armccurdy@gmail.com>
2020-12-03 10:51:08 -0800

    drop additional NULL pointer checks etc when SAFEC_DUMMY_API is defined
    It's not clear that an inlined partial implementation of the safec
    pointer or buffer size checks is a good approach (e.g. it may
    prevent static analysis at build time).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/cosa/include/safec_lib_common.h
+++ source/cosa/include/safec_lib_common.h
@@ -51,70 +51,52 @@ typedef int errno_t;
 #define ESLEMAX          403       /* length exceeds RSIZE_MAX    */
 #define ESNOSPC          406       /* not enough space for dest     */
 
-#define strcpy_s(dst,max,src) ((src) != NULL)?((max > strlen(src))?EOK:ESLEMAX):ESNULLP; \
- if(((src) != NULL) && (max > strlen(src))) strcpy(dst,src);
+#define strcpy_s(dst,max,src) EOK; \
+ strcpy(dst,src);
 
-#define strncpy_s(dst,max,src,len) ((src) != NULL)?((len <= max)?EOK:ESLEMAX):ESNULLP; \
- if(((src) != NULL) && (len <= max)) strncpy(dst,src,len);
+#define strncpy_s(dst,max,src,len) EOK; \
+ strncpy(dst,src,len);
 
 #define memset_s(dst,max_1,c,max) EOK; \
  memset(dst,c,max);
 
-#define strcat_s(dst,max,src) ((src) != NULL)?((max > strlen(src))?EOK:ESLEMAX):ESNULLP; \
- if(((src) != NULL) && (max > strlen(src))) strcat(dst,src);
+#define strcat_s(dst,max,src) EOK; \
+ strcat(dst,src);
 
-#define strncat_s(dst,max,src,len) ((src) != NULL)?((len <= max)?EOK:ESLEMAX):ESNULLP; \
- if(((src) != NULL) && (len <= max)) strncat(dst,src,len);
+#define strncat_s(dst,max,src,len) EOK; \
+ strncat(dst,src,len);
 
-#define memcpy_s(dst,max,src,len)  ((src) != NULL)?((len <= max)?EOK:ESLEMAX):ESNULLP; \
- if(((src) != NULL) && (len <= max)) memcpy(dst,src,len);
+#define memcpy_s(dst,max,src,len) EOK; \
+ memcpy(dst,src,len);
 
-#ifndef STRCPY_S_NOCLOBBER
- #define STRCPY_S_NOCLOBBER(dst,max,src) ((src) != NULL)?((max > strlen(src))?EOK:ESLEMAX):ESNULLP; \
-  if(((src) != NULL) && (strlen(src) < max)) strcpy(dst, src);
-#endif
+#define STRCPY_S_NOCLOBBER(dst,max,src) EOK; \
+ strcpy(dst, src);
 
-#define MEMCPY_S_NOCLOBBER(dst,max,src,len) ((src) != NULL) ? ((len <= max)?EOK:ESLEMAX):ESNULLP; \
-  if(((src) != NULL) && (len <= max)) memcpy(dst, src, len);
+#define MEMCPY_S_NOCLOBBER(dst,max,src,len) EOK; \
+ memcpy(dst, src, len);
 
 #define sscanf_s( buffer, fmt, ...)  EOK; \
   sscanf( buffer, fmt, ##__VA_ARGS__ );
 
 #define strtok_s(dest, dmax, delim, ptr) strtok_r(dest, delim, ptr)
 
-#define sprintf_s( dst, max, fmt, ... ) parseFormat(dst, max, fmt, ##__VA_ARGS__)
-
-static inline int parseFormat(const char *dst, int max, const char *fmt, ...)
+static inline int sprintf_s (char *str, size_t size, const char *fmt, ...)
 {
     va_list argp;
-    int len = 0;
+    int len;
 
-    if((fmt == NULL) || (dst == NULL) || (max == 0))
-    {
-        return -ESNULLP;
-    }
-
-    va_start(argp, fmt);
-
-    len = vsnprintf((char *)dst, (size_t)max, fmt, argp);
-
-    va_end(argp);
-
-    return (max > len) ? len : -ESNOSPC;
+    va_start (argp, fmt);
+    len = vsnprintf (str, size, fmt, argp);
+    va_end (argp);
+    return len;
 }
 
 static inline int strcmp_s(const char *dst, int dmax, const char *src, int *r) {
-        if((src ==  NULL) || (dst == NULL) || (dmax == 0))
-            return ESNULLP;
-
         *r = strcmp(dst, src);
         return EOK;
 }
 
 static inline int strcasecmp_s(const char *dst, int dmax, const char *src, int *r) {
-         if((src ==  NULL) || (dst == NULL) || (dmax == 0))
-            return ESNULLP;
-
          *r = strcasecmp(dst, src);
          return EOK;
 }
