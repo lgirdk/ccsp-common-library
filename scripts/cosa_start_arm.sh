fa6ec92172311e37fa824c47a7362a9fdc466999
Andre McCurdy <armccurdy@gmail.com>
2020-12-08 17:17:19 -0800

    cosa_start_arm.sh: add bootup marker
    Create marker in logfile to show when cosa service starts.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- scripts/cosa_start_arm.sh
+++ scripts/cosa_start_arm.sh
@@ -39,6 +39,8 @@ source /etc/utopia/service.d/log_env_var.sh
 source /etc/utopia/service.d/log_capture_path.sh
 source /etc/device.properties
 
+print_uptime "cosa_service_start"
+
 if [ -f /etc/mount-utils/getConfigFile.sh ];then
    . /etc/mount-utils/getConfigFile.sh
 fi
