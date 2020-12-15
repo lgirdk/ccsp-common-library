f7a6ef70780e29a89dacf351f967edb85c3f2792
Andre McCurdy <armccurdy@gmail.com>
2020-12-15 11:31:48 -0800

    util_api: disable SSLv3 in openssl_init()
    Depending on the openssl version or how it was configured, this may
    be the default already.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/util_api/ansc/AnscPlatform/user_openssl.c
+++ source/util_api/ansc/AnscPlatform/user_openssl.c
@@ -226,7 +226,13 @@ int openssl_init (int who_calls)
     return 0;
   }
 
-  SSL_CTX_set_options(ssl_ctx,SSL_OP_NO_SSLv2);
+  /*
+     SSL_OP_NO_SSLv2 is set by default for later versions of openssl 1.0.2 (and
+     all versions of 1.1.x). Add SSL_OP_NO_SSLv3 explicitly. Eventually
+     SSL_OP_NO_TLSv1 and SSL_OP_NO_TLSv1_1 should probably be set too (ie force
+     TLSv1_2) ?
+  */
+  SSL_CTX_set_options(ssl_ctx, (SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3));
 
   if (who_calls == SSL_CLIENT_CALLS)
   {
