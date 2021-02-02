bb1105a94bf44f836368e713d36f5cb6012bb45c
Andre McCurdy <armccurdy@gmail.com>
2021-02-02 15:34:40 -0800

    define _ANSC_LITTLE_ENDIAN_ automatically
    Don't require every component which needs to directly or indirectly
    include a ccsp header file to correctly define _ANSC_LITTLE_ENDIAN_
    
    Consistently use #if defined(_ANSC_LITTLE_ENDIAN_) instead
    of #ifdef _ANSC_LITTLE_ENDIAN_
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/util_api/asn.1/components/al_pki/al_cert_api.c
+++ source/util_api/asn.1/components/al_pki/al_cert_api.c
@@ -1734,7 +1734,7 @@ ALCertAcquireIDByHandle
         /*
          *  DH  AL_CERT user requires keys in host order
          */
-#if defined(_ANSC_LITTLE_ENDIAN_) && !defined(_COSA_INTEL_XB3_ARM_)
+#if defined(_ANSC_LITTLE_ENDIAN_)
         AnscMemoryFromNToH((char*)inPublicKey->Key.RSAPublicKey.Modulus.Data,                  inPublicKey->Key.RSAPublicKey.Modulus.Length);
         AnscMemoryFromNToH((char*)inPublicKey->Key.RSAPublicKey.PublicExponent.Data.ucData,    inPublicKey->Key.RSAPublicKey.PublicExponent.Length);
 
@@ -1773,7 +1773,7 @@ ALCertAcquireIDByHandle
         /*
          *  DH  AL_CERT user requires keys in host order
          */
-#if defined(_ANSC_LITTLE_ENDIAN_) && !defined(_COSA_INTEL_XB3_ARM_)
+#if defined(_ANSC_LITTLE_ENDIAN_)
         AnscMemoryFromNToH((char*)inPublicKey->Key.DSAPublicKey.ParamP.Data.ucData,    inPublicKey->Key.DSAPublicKey.ParamP.Length);
         AnscMemoryFromNToH((char*)inPublicKey->Key.DSAPublicKey.ParamQ.Data.ucData,    inPublicKey->Key.DSAPublicKey.ParamQ.Length);
         AnscMemoryFromNToH((char*)inPublicKey->Key.DSAPublicKey.ParamG.Data.ucData,    inPublicKey->Key.DSAPublicKey.ParamG.Length);
