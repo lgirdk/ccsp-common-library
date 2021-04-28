138f34464785e87c8892521a8f9f11a206e3544f
Andre McCurdy <armccurdy@gmail.com>
2021-04-27 20:51:25 -0700

    Alias Manager: re-implement with gperf based mapping tables and new LGI specific APIs
    Alias Manager: add source for .xml to gperf host tool
    
    Needs to be compiled for the host system before the main build
    process.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Alias Manager: add support for new LGI gperf based APIs
    
    Alias Manager: use separate gperf table for internal->external and external->internal
    
    Try to make the code smaller and faster.
    
    Alias Manager: fix dmcli crash during GPV ( OFW-1299 )
    
    Alias Manager: add new API's to LGI alias mapper to get list of alias names
    
    Alias Manager: modify host tool .xml to gperf to support multiple alias names
    
    Alias Manager: fix empty string check in hosttools convert_alias_xml.c
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    Alias Manager: drop "Device." prefix from input to gperf lookup tables
    
    All input strings start with "Device." so the prefix can be removed.
    Note that the prefix is still included in the output strings to
    support Alias Manager APIs which return a pointer to a string inside
    the gperf lookup tables.

---
+++ source/ccsp/components/CCSP_AliasMgr_hosttools/convert_alias_xml.c
@@ -0,0 +1,397 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//=============================================================================
+
+#define CUSTOM_MAP_GPERF_EXT2INT "custom_map_alias_ext2int.gperf"
+#define CUSTOM_MAP_GPERF_INT2EXT "custom_map_alias_int2ext.gperf"
+#define BUF_SIZE 256
+#define MAX_ALIAS 5
+#define DM_START_STR "Device."
+//=============================================================================
+
+typedef struct Alias_t
+{
+    char aliasExtName[256];
+    char aliasIntName[MAX_ALIAS][256];
+    unsigned char aliasCount;
+    unsigned char aliasStrict;
+}Alias_t;
+
+static Alias_t g_AliasList[10000];
+
+static int alias_ptr = 0;
+
+//=============================================================================
+
+static int WriteResultToFile_ext2int()
+{
+    static const char* h_declaration =
+        "%{\n" \
+        "#include <stdio.h>\n" \
+        "#include <string.h>\n\n" \
+        "#define MAX_ALIAS 5\n" \
+        "typedef struct Alias_ext_t\n" \
+        "{\n" \
+        "    char *name;\n" \
+        "    char *aliasName[MAX_ALIAS];\n" \
+        "    unsigned char aliasCount;\n" \
+        "    unsigned char aliasStrict;\n" \
+        "}Alias_ext_t; \n" \
+        "%}\n";
+
+    char temp_buf[1024];
+    int i;
+
+    // Create gperf file
+    FILE* fp = fopen(CUSTOM_MAP_GPERF_EXT2INT, "wt");
+    if (fp == NULL)
+    {
+        printf("%s: Error: Unable to open file %s\n", __FUNCTION__, CUSTOM_MAP_GPERF_EXT2INT);
+        return -1;
+    }
+    // gperf section: declarations
+    fputs(h_declaration, fp);
+    fputs("struct Alias_ext_t;\n", fp);
+
+    // gperf section: keywords
+    fputs("%%\n", fp);
+    for (i = 0; i < alias_ptr; i++)
+    {
+        // External -> Internal Mapping
+        snprintf(temp_buf, sizeof(temp_buf), "\"%s\",{\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"},%d,%d\n", g_AliasList[i].aliasExtName + strlen(DM_START_STR), g_AliasList[i].aliasIntName[0], g_AliasList[i].aliasIntName[1], g_AliasList[i].aliasIntName[2], g_AliasList[i].aliasIntName[3], g_AliasList[i].aliasIntName[4], g_AliasList[i].aliasCount, g_AliasList[i].aliasStrict);
+        fputs(temp_buf, fp);
+    }
+    fputs("%%\n", fp);
+
+    // gperf section: functions (currently it is in seperate file)
+
+    fclose(fp);
+    return 0;
+}
+
+//=============================================================================
+
+static int WriteResultToFile_int2ext()
+{
+    static const char* h_declaration =
+        "%{\n" \
+        "#include <stdio.h>\n" \
+        "#include <string.h>\n\n" \
+        "#define MAX_ALIAS 5\n" \
+        "typedef struct Alias_int_t\n" \
+        "{\n" \
+        "    char *name;\n" \
+        "    char *aliasName[MAX_ALIAS];\n" \
+        "    unsigned char aliasCount;\n" \
+        "    unsigned char aliasStrict;\n" \
+        "}Alias_int_t; \n" \
+        "%}\n";
+
+    char temp_buf[1024];
+    int i;
+
+    // Create gperf file
+    FILE* fp = fopen(CUSTOM_MAP_GPERF_INT2EXT, "wt");
+    if (fp == NULL)
+    {
+        printf("%s: Error: Unable to open file %s\n", __FUNCTION__, CUSTOM_MAP_GPERF_INT2EXT);
+        return -1;
+    }
+    // gperf section: declarations
+    fputs(h_declaration, fp);
+    fputs("struct Alias_int_t;\n", fp);
+
+    // gperf section: keywords
+    fputs("%%\n", fp);
+    for (i = 0; i < alias_ptr; i++)
+    {
+        // Internal -> External Mapping
+        snprintf(temp_buf, sizeof(temp_buf), "\"%s\",{\"%s\",\"\",\"\",\"\",\"\",},%d,%d\n", g_AliasList[i].aliasIntName[0]+ strlen(DM_START_STR), g_AliasList[i].aliasExtName, g_AliasList[i].aliasCount, g_AliasList[i].aliasStrict);
+        fputs(temp_buf, fp);
+    }
+    fputs("%%\n", fp);
+
+    // gperf section: functions (currently it is in seperate file)
+
+    fclose(fp);
+    return 0;
+}
+
+//=============================================================================
+
+static int updateAliasMap(unsigned char aliasStrict, char* paramStr1, char* paramStr2)
+{
+    char inStr[BUF_SIZE];
+    char outStr[BUF_SIZE];
+    int processDone = 0;
+
+    char* pInP1 = NULL;
+    char* pInP2 = NULL;
+    char* pOutP1 = NULL;
+    char* pOutP2 = NULL;
+    int i;
+
+    pInP1 = paramStr1;
+    pOutP1 = paramStr2;
+
+    if ( (NULL == pInP1) || (NULL == pOutP1) )
+    {
+        printf("Error: Alias mapping string...");
+        return -1;
+    }
+
+    // start from 2rd node
+    pInP2 = strstr(pInP1, ".");
+    pOutP2 = strstr(pOutP1, ".");
+    if ( (NULL == pInP2) || (NULL == pOutP2) )
+    {
+        printf("Error: Alias mapping string...");
+        return -1;
+    }
+    pInP2++;
+    pOutP2++;
+
+    while (1)
+    {
+        size_t  len;
+        int skip_update;
+
+        skip_update = 0;
+        if ((NULL == pInP2) || (NULL == pOutP2))
+        {
+            break;
+        }
+
+        inStr[0] = '\0';
+        len = pInP2 - pInP1;
+        pInP2 = strstr(pInP2, ".");
+        if (NULL == pInP2)
+        {
+            if (strlen (paramStr1) > len)
+            {
+                strcpy (inStr, paramStr1);
+                processDone = 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            pInP2++;
+            memcpy(inStr, pInP1, pInP2 - pInP1);
+            inStr[pInP2 - pInP1] = '\0';
+        }
+        outStr[0] = '\0';
+        len = pOutP2 - pOutP1;
+        pOutP2 = strstr(pOutP2, ".");
+        if (NULL == pOutP2)
+        {
+            if (strlen (paramStr2) > len)
+            {
+                strcpy (outStr, paramStr2);
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            pOutP2++;
+            memcpy(outStr, pOutP1, pOutP2 - pOutP1);
+            outStr[pOutP2 - pOutP1] = '\0';
+        }
+        if ((!aliasStrict) && (!strcmp(inStr, outStr)))
+            continue;
+        for (i = 0; i < alias_ptr; i++)
+        {
+            if (!strcmp(g_AliasList[i].aliasExtName, inStr))
+            {
+                if (aliasStrict)
+                {
+                    g_AliasList[i].aliasStrict = aliasStrict;
+                }
+                if (NULL != outStr)
+                {
+                    int j;
+
+                    for (j = 0; j < MAX_ALIAS; j++)
+                    {
+                        if (!strcmp(g_AliasList[i].aliasIntName[j], outStr))break;
+                        if (g_AliasList[i].aliasIntName[j][0] == 0)
+                        {
+                            strcpy(g_AliasList[i].aliasIntName[j], outStr);
+                            g_AliasList[i].aliasCount++;
+                            break;
+                        }
+                    }
+                }
+                skip_update = 1;
+                break;
+            }
+        }
+        if (skip_update)
+            continue;
+        if ((inStr[0] != '\0') && (outStr[0] != '\0'))
+        {
+            for (i = 0 ; i < MAX_ALIAS ; i++)
+                g_AliasList[alias_ptr].aliasIntName[i][0] = 0; 
+            strcpy (g_AliasList[alias_ptr].aliasExtName, inStr);
+            strcpy(g_AliasList[alias_ptr].aliasIntName[0], outStr);
+            g_AliasList[alias_ptr].aliasCount = 1;
+            g_AliasList[alias_ptr].aliasStrict = aliasStrict;
+            alias_ptr++;
+        }
+        if (processDone == 1) break;
+    }
+    return 0;
+}
+
+//=============================================================================
+
+static void remove_spaces(char* str)
+{
+    int count = 0;
+    int i;
+
+    for (i = 0; str[i]; i++)
+        if (str[i] != ' ')
+            str[count++] = str[i];
+    str[count] = '\0';
+}
+
+//=============================================================================
+
+int main(int argc, char* argv[])
+{
+    int state = 0;
+    unsigned char aliasStrictFlag = 0;
+    char aliasExtStr[BUF_SIZE];
+    char aliasIntStr[BUF_SIZE];
+    char temp_buf[BUF_SIZE];
+
+    if (argc != 2)
+    {
+        printf("Error: Invalid argument...\n");
+        return -1;
+    }
+
+    // open custom mapper xml file for alias mapping
+    FILE* fp = fopen(argv[1], "rt");
+    if (fp == NULL)
+    {
+        printf("Error: Unable to open input custom mapper file (%s)...", argv[1]);
+        return -1;
+    }
+
+    while (0 == feof(fp))
+    {
+        if (fgets(temp_buf, sizeof(temp_buf), fp) == NULL)
+            break;
+        remove_spaces(temp_buf);
+        if (temp_buf[strlen(temp_buf) - 1] == '\n')
+        {
+            temp_buf[strlen(temp_buf) - 1] = '\0';
+        }
+        switch (state)
+        {
+            case 0:
+            {
+                if (strcmp(temp_buf, "<ParamList>") == 0)
+                {
+                    state = 1;
+                }
+                break;
+            }
+            case 1:
+            {
+                if (strcmp(temp_buf, "<AliasList>") == 0)
+                {
+                    aliasExtStr[0] = '\0';
+                    aliasIntStr[0] = '\0';
+                    aliasStrictFlag = 0;
+                    state = 2;
+                }
+            }
+            break;
+            case 2:
+            {
+                char* p1, * p2;
+                p1 = strstr(temp_buf, "<Aliasstrict=");
+
+                if (p1 != NULL)
+                {
+                    if (0 == strcmp(temp_buf, "<Aliasstrict=\"true\">"))
+                    {
+                        aliasStrictFlag = 1;
+                    }
+                    break;
+                }
+                p1 = strstr(temp_buf, "<External>");
+                if (p1 != NULL)
+                {
+                    p1 += strlen("<External>");
+                    p2 = strstr(p1, "</External>");
+                    if ((p1 != NULL) && (p2 != NULL))
+                    {
+                        memcpy (aliasExtStr, p1, p2 - p1);
+                        aliasExtStr[p2 - p1] = '\0';
+                    }
+                    else
+                    {
+                        //invalid xml tag.
+                        printf("Error: Invalid xml tag (External)...\n");
+                    }
+                    break;
+                }
+                p1 = strstr(temp_buf, "<Internal>");
+                if (p1 != NULL)
+                {
+                    p1 += strlen("<Internal>");
+                    p2 = strstr(p1, "</Internal>");
+                    if ((p1 != NULL) && (p2 != NULL))
+                    {
+                        memcpy(aliasIntStr, p1, p2 - p1);
+                        aliasIntStr[p2 - p1] = '\0';
+                    }
+                    else
+                    {
+                        //invalid xml tag.
+                        printf("Error: Invalid xml tag (Internal)... \n");
+                    }
+                }
+                if ((aliasExtStr[0] != '\0') && (aliasIntStr[0] != '\0'))
+                {
+                    if (0 != updateAliasMap(aliasStrictFlag, aliasExtStr, aliasIntStr))
+                    {
+                        printf("Error: updating to map list...");
+                    }
+                    aliasExtStr[0] = '\0';
+                    aliasIntStr[0] = '\0';
+                    aliasStrictFlag = 0;
+                }
+            }
+            break;
+            default:
+                break;
+        }
+    }
+    printf("Alias map count = %d\n", alias_ptr);
+    if (0 != WriteResultToFile_ext2int())
+    {
+        printf ("Error: Creating gperf file (%s)...\n", CUSTOM_MAP_GPERF_EXT2INT);
+    }
+    if (0 != WriteResultToFile_int2ext())
+    {
+        printf("Error: Creating gperf file (%s)...\n", CUSTOM_MAP_GPERF_INT2EXT);
+    }
+    return 0;
+}
+
+//=============================================================================
+
