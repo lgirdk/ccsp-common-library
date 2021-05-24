1fa8a6634c11c794dfc7701c2af78bc3556bc4ed
anil shetty <adshetty@libertyglobal.com>
2021-05-24 01:32:24 -0700

    Alias Manager: add new API's to LGI alias mapper to get list of alias names

--- source/ccsp/components/include/custom_alias_utils.h
+++ source/ccsp/components/include/custom_alias_utils.h
@@ -1,14 +1,21 @@
 #ifndef  __CUSTOM_ALIAS_UTILS_H__
 #define  __CUSTOM_ALIAS_UTILS_H__
 
-typedef struct Alias_t
+#define MAX_ALIAS 5
+
+typedef struct aliasNames_t
 {
-    char *name;
-    char *aliasName;
-    unsigned char aliasStrict;
-}Alias_t;
+    char* aliasName[MAX_ALIAS];
+    unsigned char aliasCount;
+}aliasNames_t;
 
 const char* aliasGetInternalName(char* externalName, int* releaseMem);
 const char* aliasGetExternalName(char* internalName, int* releaseMem);
+
+const char* lgiAliasGetInternalName(char* externalName);
+const char* lgiAliasGetExternalName(char* internalName);
+aliasNames_t* lgiAliasGetInternalNames(char* externalName);
+const char* lgiAliasGetNextName(struct aliasNames_t* pAliasNames);
+void lgiAliasFreeNamesList(struct aliasNames_t* pAliasNames);
 
 #endif
