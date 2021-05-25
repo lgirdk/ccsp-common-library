ba21e28b5b6d00841c47c449569b1c5865c6d96f
Andre McCurdy <armccurdy@gmail.com>
2021-05-24 17:16:52 -0700

    drop unused AnscCreateScanner(), AnscRemoveScanner() and AnscScanString() APIs
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/debug_api/ansc_wrapper_api.h
+++ source/debug_api/ansc_wrapper_api.h
@@ -353,27 +353,6 @@ AnscCloneString
 #endif
 
 #if 0
-ANSC_HANDLE
-AnscCreateScanner
-    (
-        char**                      string_array,
-        ULONG                       string_count
-    );
-
-ANSC_STATUS
-AnscRemoveScanner
-    (
-        ANSC_HANDLE                 hStringScanner
-    );
-
-char*
-AnscScanString
-    (
-        ANSC_HANDLE                 hStringScanner,
-        char*                       tbs_string
-    );
-#endif    
-#if 0
 BOOL
 AnscIsValidIpString
     (
