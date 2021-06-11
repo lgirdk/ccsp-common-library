b53c9863a448f528cf6f7b8e7df6d81a53dfe3df
BijeeshaVS <bvattakaithayilsudha.contractor@libertyglobal.com>
2021-06-11 14:05:30 +0200

    increase message bus timeout again, from 300 -> 360 seconds for Puma6 targets ( OFW-699 )
    Fix CcspPandM dead lock due to bus timeout seen during TR-069 execution.
    
    BUG DESCRIPTION:-
    
    The TR-069 execution has been run a couple of times on the CDRouter and random CcspPandMSsp dead lock is seen during the execution.
    
    FIX DESCRIPTION:-
    
    Did analysis by enabling debug logs and found that Device.Ethernet.Interface. query is taking around 1 minute.
    
    Increased time out value to avoid random dead locks.
    
    TESTING PERFORMED:-
    
    1. Verified Device. query from ACS and dmcli.
    2. Executed the affected Tr069 testcases on loop and verified test cases are passing.

--- source/util_api/ccsp_msg_bus/ccsp_base_api.c
+++ source/util_api/ccsp_msg_bus/ccsp_base_api.c
@@ -74,9 +74,9 @@ typedef struct _component_info {
 } component_info;
 
 #if defined(PUMA6_PLATFORM)
-/*Because of more CPU load in PUMA6 devices timeout is set to 5 minutes*/
-int   CcspBaseIf_timeout_seconds        = 300; //seconds
-int   CcspBaseIf_timeout_getval_seconds = 300; //seconds
+/*Because of more CPU load in PUMA6 devices timeout is set to 6 minutes*/
+int   CcspBaseIf_timeout_seconds        = 360; //seconds
+int   CcspBaseIf_timeout_getval_seconds = 360; //seconds
 #else
 int   CcspBaseIf_timeout_seconds        = 120; //seconds
 int   CcspBaseIf_timeout_getval_seconds = 120; //seconds
