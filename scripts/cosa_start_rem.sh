5b894debec4d7b78a5997550463f4d45f08de9e0
Andre McCurdy <armccurdy@gmail.com>
2021-07-28 17:49:11 -0700

    cosa_start_rem.sh: drop support for bringing up brlan10
    Related to MoCA home network isolation and not required in OFW
    images.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- scripts/cosa_start_rem.sh
+++ scripts/cosa_start_rem.sh
@@ -93,48 +93,6 @@ fi
 echo "Creating MESH vlans"
 $UTOPIA_PATH/service_multinet_exec create_mesh_vlan &
 
-HOME_LAN_ISOLATION=`psmcli get dmsb.l2net.HomeNetworkIsolation`
-if [ "$HOME_LAN_ISOLATION" = "1" ]; then
-echo "Starting brlan10 initialization, check whether brlan10 is there or not"
-ifconfig | grep brlan10
-if [ $? == 1 ]; then
-    echo "brlan10 not present go ahead and create it"
-    if [ "$BOX_TYPE" = "XF3" ]; then
-        sh /usr/ccsp/lan_handler.sh home_lan_isolation_enable
-    else
-        sysevent set multinet-up 9
-    fi
-fi
-
-# Waiting for brlan10 -MoCA bridge interface creation for 30 sec
-iter=0
-max_iter=2
-while [ ! -f /tmp/MoCABridge_up ] && [ "$iter" -le $max_iter ]
-do
-    iter=$((iter+1))
-    echo "Sleep Iteration# $iter"
-    sleep 10
-done
-
-if [ ! -f /tmp/MoCABridge_up ]; then
-    echo "brlan10 is not up after maximum iterations i.e 30 sec go ahead with the execution"
-else
-    echo "brlan10 is created after interation $iter go ahead with the execution"
-    killall igmpproxy                                   
-    killall MRD
-    sleep 1 
-    #smcroute -f /usr/ccsp/moca/smcroute.conf -d
-    if [ "$BOX_TYPE" = "XF3" ]; then
-    	sh /etc/utopia/service.d/service_mcastproxy.sh mcastproxy-restart
-    else 
-        sysevent set mcastproxy-restart
-    fi
-    MRD &
-    sysevent set firewall-restart
-
-fi
-
-fi
 echo_t "Elected subsystem is $Subsys"
 
 if [ -f /lib/rdk/start-upnp-service ]; then
