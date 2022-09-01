e924ef3baea69b8fb01a7b2fc367150eae102c7e
Andre McCurdy <armccurdy@gmail.com>
2022-09-01 13:38:17 -0700

    drop unused and obsolete references to _ANSC_ENABLE_PRAGMA_
    This option was previously used for Windows. The legacy support for
    building for Windows has now been removed.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/cosa/include/ansc_packet_binary_ipcomp.h
+++ source/cosa/include/ansc_packet_binary_ipcomp.h
@@ -75,15 +75,8 @@
 
 **********************************************************************/
 
-
 #ifndef  _ANSC_PACKET_BINARY_IPCOMP_
 #define  _ANSC_PACKET_BINARY_IPCOMP_
-
-
-#ifdef   _ANSC_ENABLE_PRAGMA_
-#pragma  warning(disable: 4200)                     /* nonstandard extension used: zero-sized array in struct/union */
-#endif
-
 
 /*
  * All network protocol messages must be defined without any packing. While most protocols were
@@ -91,9 +84,6 @@
  * boudary, there're plenty protocols didn't follow this rule. Following are two of typical ways
  * to control the byte bounary in C/C++ programs:
  */
-#ifdef   _ANSC_ENABLE_PRAGMA_
-#pragma  pack(1)
-#endif
 
 #ifdef  __GNUC__
 
@@ -146,9 +136,6 @@ IPCOMP_HEADER,  *PIPCOMP_HEADER;
 #define  AnscIpcompSetSpi(p, n)                     AnscIpcompSetCpi(p, (USHORT)n)
 
 
-#ifdef   _ANSC_ENABLE_PRAGMA_
-#pragma  pack()
-#endif
 
 
 #endif
