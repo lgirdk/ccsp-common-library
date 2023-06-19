b6f261a181f4ae38aedd6b2e7162febe40381c77
Oussema Ben Abdallah <obenabdallah@libertyglobal.com>
2023-06-19 16:16:59 +0200

    ipoe-health-check: recovery during Fiber failover ( OFW-4640 )
    [Issue]
    Implement first set of changes required for CGW-52.
    
    [Testing Performed]
    Start gateway and check in multi vlan scenario each interface is
    running a separate instance.
    Check when link is down, BFD packet fails and DHCP clients go for
    renewing their IP.
    
    Upstream Candidate: No. LGI specific.
    
    Signed-off-by: Oussema ben abdallah(obenabdallah@libertyglobal.com)

--- source/ccsp/components/include/ipc_msg.h
+++ source/ccsp/components/include/ipc_msg.h
@@ -8,6 +8,8 @@
 #define PPP_MANAGER_ADDR               "tcp://127.0.0.1:50322"
 #define IHC_IPC_ADDR               "tcp://127.0.0.1:50323"
 #define DHCP_MANAGER_ADDR              "tcp://127.0.0.1:50324"
+#define IHC_IPC_ADDR_MG0               "tcp://127.0.0.1:50325"
+#define IHC_IPC_ADDR_VOIP              "tcp://127.0.0.1:50326"
 
 #define DEFAULT_IPC_SOCKET_TIMEOUT       5000 //5 seconds
 
@@ -189,9 +191,12 @@ typedef enum
     IPOE_MSG_IHC_ECHO_FAIL_IPV4,
     IPOE_MSG_IHC_ECHO_FAIL_IPV6,
     IPOE_MSG_IHC_ECHO_RENEW_IPV4,
+    IPOE_MSG_IHC_ECHO_RENEW_MGMT,
+    IPOE_MSG_IHC_ECHO_RENEW_VOIP,
     IPOE_MSG_IHC_ECHO_RENEW_IPV6,
     IPOE_MSG_IHC_ECHO_IPV4_UP,
     IPOE_MSG_IHC_ECHO_IPV6_UP,
+    IPOE_MSG_IHC_RESTART_DUO_BINDING_ERROR,
 }ipoe_msg_type_t;
 
 typedef struct
