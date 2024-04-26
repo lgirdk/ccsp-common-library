d30a0f06e52959ad8d238ea5d4b77c3766273a47
qlei <rlei@libertyglobal.com>
2024-04-25 23:08:23 -0700

    remove dependency on dbus

--- source/util_api/ccsp_msg_bus/ccsp_base_api.c
+++ source/util_api/ccsp_msg_bus/ccsp_base_api.c
@@ -2597,46 +2597,6 @@ int CcspBaseIf_WebConfigSignal_rbus (
 }
 
 #endif 
-int CcspBaseIf_WebConfigSignal (
-    void* bus_handle,
-    char* webconfig
-)
-{
-
-    CCSP_MESSAGE_BUS_INFO *bus_info = (CCSP_MESSAGE_BUS_INFO *)bus_handle;
-    DBusMessage *message;
-    int i;
-    DBusConnection *conn = NULL ;
-
-    /*to support daemon redundency*/
-    pthread_mutex_lock(&bus_info->info_mutex);
-    for(i = 0; i < CCSP_MESSAGE_BUS_MAX_CONNECTION; i++)
-    {
-        if(bus_info->connection[i].connected && bus_info->connection[i].conn )
-        {
-            conn = bus_info->connection[i].conn;
-            break;
-        }
-
-    }
-    pthread_mutex_unlock(&bus_info->info_mutex);
-
-    if(i ==  CCSP_MESSAGE_BUS_MAX_CONNECTION)
-        return CCSP_ERR_NOT_CONNECT;
-
-
-    message = dbus_message_new_signal (CCSP_DBUS_PATH_EVENT, CCSP_DBUS_INTERFACE_EVENT, "webconfigSignal" );
-
-    if(!message)
-        return CCSP_ERR_MEMORY_ALLOC_FAIL;
-
-    dbus_message_append_args (message, DBUS_TYPE_STRING, &webconfig, DBUS_TYPE_INVALID);
-
-    dbus_connection_send (conn, message, NULL);
-
-    dbus_message_unref (message);
-    return CCSP_SUCCESS;
-}
 
 int CcspBaseIf_SendSignal(
     void * bus_handle,
